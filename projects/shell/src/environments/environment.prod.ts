5899889db8a2389af8e7cebf41c70149913b3cb4
parxomchik <dmitriy.parkhomenko@icloud.com>
2024-07-17 11:58:14 +0300

    Updated prod config

--- projects/shell/src/environments/environment.prod.ts
+++ projects/shell/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  remoteNewsMarketUrl: 'https://angular-news-micro-services-market.vercel.app',
-  remoteNewsSymbolUrl: 'https://angular-news-micro-services-symbol.vercel.app',
+  remoteNewsMarketUrl: 'https://angular-news-micro-services-market.vercel.app/remoteEntry.js',
+  remoteNewsSymbolUrl: 'https://angular-news-micro-services-symbol.vercel.app/remoteEntry.js',
 };
