d73304ae6dfad6f46c6781d296d0d975ae810cf2
parxomchik <dmitriy.parkhomenko@icloud.com>
2024-07-17 11:52:06 +0300

    Added env for vercel

---
+++ projects/shell/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  remoteNewsMarketUrl: 'https://angular-news-micro-services-market.vercel.app',
+  remoteNewsSymbolUrl: 'https://angular-news-micro-services-symbol.vercel.app',
+};
